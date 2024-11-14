2991c8a29d94059e983a2ffacb34bc91b945dfb2
Juliana-CA <julicaran@gmail.com>
2024-11-14 09:34:04 -0400

    Update testeaula9csv.py

--- testeaula9csv.py
+++ testeaula9csv.py
@@ -5,9 +5,8 @@ dfq = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/B
 dfq.drop(columns=['Unnamed: 0'], inplace=True)
 lista = ['Lat_d', 'Long_d']
 dfq[lista] = dfq[lista].apply(pd.to_numeric, errors='coerce')
-dfq.info()
 estados = dfq['NM_UF'].unique()
 estadoFiltro = st.selectbox(
     'Selecione o estado',
-     estados)
+    sorted(estados))
 dadosFiltrados = dfq[dfq['NM_UF'] == estadoFiltro]
