60565d845c71e24a01426e70cca7d57bd1bdc690
Juliana-CA <julicaran@gmail.com>
2024-11-14 10:42:45 -0400

    Update testeaula9csv.py

--- testeaula9csv.py
+++ testeaula9csv.py
@@ -5,6 +5,7 @@ dfq = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/B
 dfq.drop(columns=['Unnamed: 0'], inplace=True)
 lista = ['Lat_d', 'Long_d']
 dfq[lista] = dfq[lista].apply(pd.to_numeric, errors='coerce')
+
 estados = dfq['NM_UF'].unique()
 estadoFiltro = st.selectbox(
     'Selecione o estado',
@@ -13,16 +14,22 @@ dadosFiltrados = dfq[dfq['NM_UF'] == estadoFiltro]
 if st.checkbox('Mostrar tabela'):
   st.write(dadosFiltrados)
 st.map(dadosFiltrados, latitude="Lat_d", longitude="Long_d")
+
 st.write('Comunidades Quilombolas por estado')
 st.bar_chart(dfq['NM_UF'].value_counts())
+
 st.write('Municipios com maior número de comunidades quilombolas')
 st.bar_chart(dfq['NM_MUNIC'].value_counts()[:10])
 
 qtdeMunicipios = len(dfq['NM_MUNIC'].unique())
 st.write("Existem " + str(qtdeMunicipios) + " municípios com comunidades quilombolas")
+#opção2
+st.metric('# Municípios', len(dfq['NM_MUNIC'].unique()))
 
 qtdeComunidades = len(dfq['NM_AGLOM'].unique())
 st.write("Nestes " +str(qtdeMunicipios) + " existem " + str(qtdeComunidades) + " comunidades quilombolas")
+#opção 2
+st.metric('# Comunidades', len(dfq['NM_AGLOM'].unique()))
 
 st.header('Número de comunidades por estado')
 st.bar_chart(dfq['NM_UF'].value_counts())
